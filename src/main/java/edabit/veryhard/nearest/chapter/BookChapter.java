52b8fb5bfc66f02d6859b5320e0693b3519e3166
DicksonChris <70047029+DicksonChris@users.noreply.github.com>
2021-03-03 18:58:37 -0600

    adds Nearest Chapter challenge and tests

---
+++ src/main/java/edabit/veryhard/nearest/chapter/BookChapter.java
@@ -0,0 +1,17 @@
+package edabit.veryhard.nearest.chapter;
+
+import java.util.Arrays;
+
+// Create a function that returns which chapter is nearest to the page you're on.
+// If two chapters are equidistant, return the chapter with the higher page number.
+public class BookChapter {
+    public static String nearestChapter(Chapter[] chapter, int page) {
+        return Arrays.stream(chapter)
+                     .reduce(new Chapter("", -1), (Chapter a, Chapter b) ->
+                         Math.abs(a.getPage() - page) < Math.abs(b.getPage() - page)
+                                 ? a // Chapter A is closer to page
+                                 : b // Chapter B is closer or is equidistant and has a higher page number
+                     ).getName();
+    }
+}
+
